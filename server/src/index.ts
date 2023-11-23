a41542e6e0a1a6347a8c8506ba2227d5c253d2c4
kentakki416 <fujiken0814@outlook.jp>
2023-11-23 18:51:25 +0900

    dockerでサーバーの起動

---
+++ server/src/index.ts
@@ -0,0 +1,11 @@
+import * as express from "express";
+
+const app = express();
+
+app.listen(8080, () => {
+  console.log('Start!!')
+})
+
+app.get("/", (_, res) => {
+  return res.send("Hello World");
+});
