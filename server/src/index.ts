baca2cc7cab7f8a6f37013bb9800bc466937c4eb
kentakki416 <fujiken0814@outlook.jp>
2024-03-02 22:09:42 +0900

    fix:lintの修正対応

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,9 +3,9 @@ import * as express from 'express';
 const app = express();
 
 app.listen(8080, () => {
-  console.log('Start!!')
+  // console.log('Start!!')
 })
 
-app.get("/", (_, res) => {
+app.get('/', (_, res) => {
   return res.send('Hello World');
 });
