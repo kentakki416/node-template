7d0e0eae42b910549744d07613edeff892339628
Nagisa Kaworu <fujimori_kenta@cyberagent.co.jp>
2024-05-12 22:51:47 +0900

    fix:動的にAPIのバリデーションを行えるように修正

--- server/src/infrastructure/api_schema/index.ts
+++ server/src/infrastructure/api_schema/index.ts
@@ -6,3 +6,5 @@ import { userSchemas } from './user'
 export const allSchema = {
   ...userSchemas
 }
+
+export type Route = keyof typeof allSchema
--- server/src/infrastructure/middleware/http_validate.ts
+++ server/src/infrastructure/middleware/http_validate.ts
@@ -1,19 +1,31 @@
 import Ajv from 'ajv'
 import { Request, Response, NextFunction } from 'express'
 
-import { allSchema } from '../api_schema'
+import { allSchema, type Route } from '../api_schema'
+import type { IHttpValidate } from './http_validate_interface'
+import type { ILogger } from '../log/i_logger'
 
-export class HttpValidte {
+export class HttpValidte implements IHttpValidate {
   private _ajv: Ajv
-  constructor() {
+  private _logger: ILogger
+  constructor(logger: ILogger) {
     this._ajv = new Ajv()
+    this._logger = logger
   }
 
   public middleware() {
     return (req: Request, res: Response, next: NextFunction) => {
+      const route: Route = `${req.method} ${req.path}` as Route
+      const schema = allSchema[route]
+      if (!schema) {
+        this._logger.error(new Error(`schema is not defined. method:${req.method}, path:${req.path}`))
+        next();
+        return
+      }
       const validate = this._ajv.compile(allSchema)
       const valid = validate(req)
       if (!valid) {
+        this._logger.error(new Error(`Validation Error ${validate.errors}`))
         res.status(400).json({
           message: 'Invalid request body',
           errors: validate.errors
